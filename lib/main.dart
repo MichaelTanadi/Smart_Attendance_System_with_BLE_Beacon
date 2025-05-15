f14d91f6d95bbf1aaa8529ee68c1684bcedad64a
MichaelTanadi <133932545+MichaelTanadi@users.noreply.github.com>
2025-05-15 21:28:34 +0800

    Update title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Smart Attendance',
+      title: 'Smart Attendance System',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
